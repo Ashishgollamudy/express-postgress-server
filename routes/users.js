315d18ee97612f9822c287ca4e9fa360ef247bb6
Ashish Gollamudy <Ashish Gollamudy>
2017-08-21 21:21:34 -0400

    making db changes

--- routes/users.js
+++ routes/users.js
@@ -6,20 +6,19 @@ var credentials = require('../credentials/user.json');
 const cn = {
     user: credentials.user,
     password: credentials.password,
-    host: 'covelite', // 'localhost' is the default;
+    host: 'localhost', // 'localhost' is the default;
     port: 5432, // 5432 is the default;
-    database: 'organization_item_masters'
+    database: 'postgres'
 };
 
 const db = pgp(cn);
 
 var response;
 
-
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
-        db.one('select * from organization_item_masters_rjr_agg_week_2017_04_23 limit 10', [true])
+        db.any('select * from users', [true])
             .then(function (data) {
                 res.send({response:data});
             })
@@ -27,5 +26,39 @@ router.get('/', function(req, res, next) {
                 console.log('ERROR:', error)
             });
 });
+
+// getting a user from userId
+
+router.get('/:userId',(req,res,next) => {
+    db.one('select * from users where id =' + req.params.userId)
+       .then((data) => {
+         res.send({response:data});
+    })
+    .catch((error) =>{
+        res.send({response:'No result found'});
+        console.log('ERROR:',error)
+    });
+});
+
+// creating a user
+
+router.post('/newgame',(req,res,next) => {
+    
+    const params = {
+        id:req.body.id,
+        name:req.body.name
+    }
+    
+    db.none('INSERT INTO users(id, name) VALUES(${id}, ${name})',params)
+    .then(() => {
+        res.send({response:'Added a new user successfully'});
+    })
+    .catch(error => {
+        res.send({response:error.detail});
+        console.log(error);
+    });
+    
+});
+
 
 module.exports = router;
